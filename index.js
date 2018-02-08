bd804a1ba1a80d1ddf6e2b48d29e69e9353be91d
JimmyLin39 <jimmylin0309@email.com>
2018-02-07 16:46:51 -0800

    add react-redux

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
 import App from './App';
-AppRegistry.registerComponent('reactnativecourse', () => App);
+import store from './src/store/store';
+
+const reduxStore = store();
+
+const reactNativeRedux = () => (
+  <Provider store={reduxStore}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent('reactnativecourse', () => reactNativeRedux);
